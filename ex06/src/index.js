4c48afdd047b1edfe4ef346fe8902d1bd3f88f44
abidmujkic <abidmujkic1@gmail.com>
2021-09-29 09:04:45 +0200

    day16

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+exports.firstName = (firstName) => {
+    return firstName.toUpperCase();
+}
+exports.lastName = (lastName) => {
+    return lastName.toLowerCase();
+}
+console.log(this.firstName("marlboro red core"));
+console.log(this.lastName("DAVIDOFF CLASSIC"));
